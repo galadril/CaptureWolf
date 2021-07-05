87b364c32e788c0c9b48307860f09b9c26f67949
HNO Mobile <heinis.mark@gmail.com>
2021-07-05 10:42:35 +0200

    Capture the wolves

---
+++ CaptureWolf/Program.cs
@@ -0,0 +1,277 @@
+﻿using Gma.System.MouseKeyHook;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace CaptureWolf
+{
+    /// <summary>
+    /// Defines the <see cref="Program" />.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Defines the SwMinimize.
+        /// </summary>
+        internal const Int32 SwMinimize = 6;
+
+        /// <summary>
+        /// Defines the HwndBroadcast.
+        /// </summary>
+        private const int HwndBroadcast = 0xFFFF;
+
+        /// <summary>
+        /// Defines the ScMonitorpower.
+        /// </summary>
+        private const int ScMonitorpower = 0xF170;
+
+        /// <summary>
+        /// Defines the ShutOffDisplay.
+        /// </summary>
+        private const int ShutOffDisplay = 2;
+
+        /// <summary>
+        /// Defines the WmSyscommand.
+        /// </summary>
+        private const int WmSyscommand = 0x0112;
+
+        /// <summary>
+        /// Defines the _onlyOnce.
+        /// </summary>
+        private static bool _onlyOnce;
+
+        /// <summary>
+        /// Defines the camera.
+        /// </summary>
+        private static WebCam camera;
+
+        /// <summary>
+        /// Defines the ExecutionState.
+        /// </summary>
+        [FlagsAttribute]
+        public enum ExecutionState : uint
+        {
+            /// <summary>
+            /// Defines the EsAwaymodeRequired.
+            /// </summary>
+            EsAwaymodeRequired = 0x00000040,
+            /// <summary>
+            /// Defines the EsContinuous.
+            /// </summary>
+            EsContinuous = 0x80000000,
+            /// <summary>
+            /// Defines the EsDisplayRequired.
+            /// </summary>
+            EsDisplayRequired = 0x00000002,
+            /// <summary>
+            /// Defines the EsSystemRequired.
+            /// </summary>
+            EsSystemRequired = 0x00000001
+        }
+
+        /// <summary>
+        /// The SetThreadExecutionState.
+        /// </summary>
+        /// <param name="esFlags">The esFlags<see cref="ExecutionState"/>.</param>
+        /// <returns>The <see cref="ExecutionState"/>.</returns>
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        internal static extern ExecutionState SetThreadExecutionState(ExecutionState esFlags);
+
+        /// <summary>
+        /// Catch the wolf!!.
+        /// </summary>
+        private static void CatchWolf()
+        {
+            if (_onlyOnce)
+                return;
+            _onlyOnce = true;
+
+            Console.WriteLine("Capturing a WOLF!!!");
+            LockWorkStation();
+            TakeSnapshot();
+            PreventScreenSaver(false);
+
+            camera.Stop();
+            Environment.Exit(0);
+        }
+
+        /// <summary>
+        /// The GetConsoleWindow.
+        /// </summary>
+        /// <returns>The <see cref="IntPtr"/>.</returns>
+        [DllImport("Kernel32.dll", CallingConvention = CallingConvention.StdCall, SetLastError = true)]
+        private static extern IntPtr GetConsoleWindow();
+
+        /// <summary>
+        /// The HookupEvents.
+        /// </summary>
+        private static void HookupEvents()
+        {
+            Hook.GlobalEvents().MouseMove += (sender, e) =>
+            {
+                CatchWolf();
+            };
+            Hook.GlobalEvents().KeyUp += (sender, e) =>
+            {
+                if (e.Shift && Keys.W == e.KeyCode)
+                    return;
+                CatchWolf();
+            };
+        }
+
+        /// <summary>
+        /// The Init.
+        /// </summary>
+        /// <param name="silent">The silent<see cref="bool"/>.</param>
+        private static void Init(bool silent)
+        {
+
+            Console.WriteLine("    @(#                   #(       ");
+            Console.WriteLine("    #.((# *(((((((((((@@@((*       ");
+            Console.WriteLine("    # .%(((#(((((((((@(((&.        ");
+            Console.WriteLine("    / #///////////////////(&#      ");
+            Console.WriteLine("   .//////////////////////////     ");
+            Console.WriteLine("   (/////     /////&    /////%     ");
+            Console.WriteLine(" &///////. %@  /#//  @@ ///////(   ");
+            Console.WriteLine("  (/////////&  &//  (,/////////    ");
+            Console.WriteLine("   &,         .///*          @     ");
+            Console.WriteLine("    , .    .  (@@&@  , *           ");
+            Console.WriteLine("      .., * ,  @*,, (.%# .#..     ");
+            Console.WriteLine("             .,@#@&                ");
+            Console.WriteLine("              .   ,,               ");
+            Console.WriteLine("                                   ");
+            if (silent) return;
+
+            Console.WriteLine("Are you ready to capture/lure some wolves?? (Y/N)");
+            var response = Console.ReadLine();
+            if (string.Compare(response, "N", StringComparison.OrdinalIgnoreCase) != 0) return;
+            Environment.Exit(0);
+            return;
+        }
+
+        /// <summary>
+        /// Lock workstation.
+        /// </summary>
+        [DllImport("user32.dll")]
+        private static extern void LockWorkStation();
+
+        /// <summary>
+        /// The main program class.
+        /// </summary>
+        /// <param name="args">The args<see cref="string[]"/>.</param>
+        private static void Main(string[] args)
+        {
+            var silent = args is { Length: > 0 } && !string.IsNullOrEmpty(args[0]) && (string.Compare(args[0], "/silent", StringComparison.OrdinalIgnoreCase) == 0 ||
+                string.Compare(args[0], "/s", StringComparison.OrdinalIgnoreCase) == 0);
+
+            PreventScreenSaver(true);
+
+            Init(silent);
+
+            MinimizeAll();
+            HookupEvents();
+            Application.Run(new ApplicationContext());
+        }
+
+        /// <summary>
+        /// Minimize all windows.
+        /// </summary>
+        private static void MinimizeAll()
+        {
+            var thisProcess =
+                Process.GetCurrentProcess();
+            var processes =
+                Process.GetProcesses();
+            foreach (var process in processes)
+            {
+                if (process == thisProcess) continue;
+                var handle = process.MainWindowHandle;
+                if (handle == System.IntPtr.Zero) continue;
+                ShowWindow(handle, SwMinimize);
+            }
+        }
+
+        /// <summary>
+        /// Minimize this windows.
+        /// </summary>
+        private static void MinimizeConsoleWindow()
+        {
+            var hWndConsole = GetConsoleWindow();
+            ShowWindow(hWndConsole, SwMinimize);
+        }
+
+        /// <summary>
+        /// Post a message.
+        /// </summary>
+        /// <param name="hWnd">The hWnd<see cref="IntPtr"/>.</param>
+        /// <param name="msg">The msg<see cref="uint"/>.</param>
+        /// <param name="wParam">The wParam<see cref="IntPtr"/>.</param>
+        /// <param name="lParam">The lParam<see cref="IntPtr"/>.</param>
+        /// <returns>The <see cref="bool"/>.</returns>
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool PostMessage(IntPtr hWnd, uint msg,
+            IntPtr wParam, IntPtr lParam);
+
+        /// <summary>
+        /// Prevent screen saver or not.
+        /// </summary>
+        /// <param name="sw">The sw<see cref="bool"/>.</param>
+        private static void PreventScreenSaver(bool sw)
+        {
+            if (sw)
+                SetThreadExecutionState(ExecutionState.EsDisplayRequired | ExecutionState.EsContinuous);
+            else
+                SetThreadExecutionState(ExecutionState.EsContinuous);
+        }
+
+        /// <summary>
+        /// Set the ShowWindow value.
+        /// </summary>
+        /// <param name="hWnd">The hWnd<see cref="IntPtr"/>.</param>
+        /// <param name="nCmdShow">The nCmdShow<see cref="Int32"/>.</param>
+        /// <returns>The <see cref="bool"/>.</returns>
+        [DllImport("User32.dll", CallingConvention = CallingConvention.StdCall, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool ShowWindow([In] IntPtr hWnd, [In] Int32 nCmdShow);
+
+        /// <summary>
+        /// Take the snapshot.
+        /// </summary>
+        private static void TakeSnapshot()
+        {
+            try
+            {
+                camera = new WebCam(new Size(1280, 960), 30);
+                camera.Start();
+
+                Image capturedImage = null;
+                var counter = 0;
+                while (capturedImage == null || counter >= 5)
+                {
+                    capturedImage = camera.CurrentImage;
+                    counter++;
+                }
+
+                var location = System.Reflection.Assembly.GetEntryAssembly()?.Location;
+                if (location == null) return;
+                var dir = location[..location.LastIndexOf(@"\", StringComparison.Ordinal)] +
+                          "\\captured";
+                Directory.CreateDirectory(dir);
+                var newFile = dir + "\\wolf" +
+                              Guid.NewGuid().ToString() + ".jpg";
+                using var fs = new FileStream(newFile, FileMode.Create);
+                Console.WriteLine($"Writing image: {newFile}");
+                capturedImage.Save(fs, ImageFormat.Jpeg);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
---
+++ CaptureWolf/WebCam.cs
@@ -0,0 +1,124 @@
+﻿using AForge.Video;
+using AForge.Video.DirectShow;
+using System;
+using System.Drawing;
+using System.Linq;
+
+namespace CaptureWolf
+{
+    /// <summary>
+    /// Defines the <see cref="WebCam" />.
+    /// </summary>
+    internal class WebCam
+    {
+        /// <summary>
+        /// Defines the _frameRate.
+        /// </summary>
+        private readonly int _frameRate;
+
+        /// <summary>
+        /// Defines the CurrentImage.
+        /// </summary>
+        public Bitmap CurrentImage;
+
+        /// <summary>
+        /// Defines the _frameSize.
+        /// </summary>
+        private Size _frameSize;
+
+        /// <summary>
+        /// Defines the _videoDevices.
+        /// </summary>
+        private FilterInfoCollection _videoDevices = null;
+
+        /// <summary>
+        /// Defines the _videoSource.
+        /// </summary>
+        private VideoCaptureDevice _videoSource = null;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WebCam"/> class.
+        /// </summary>
+        /// <param name="frameSize">The frame size<see cref="Size"/>.</param>
+        /// <param name="frameRate">The frame rate<see cref="int"/>.</param>
+        public WebCam(Size frameSize, int frameRate)
+        {
+            this._frameSize = frameSize;
+            this._frameRate = frameRate;
+            this.CurrentImage = null;
+        }
+
+        /// <summary>
+        /// The Start.
+        /// </summary>
+        public void Start()
+        {
+            if (GetCamList().Count == 0)
+                throw new Exception("Video device not found");
+            
+            _videoSource = new VideoCaptureDevice(_videoDevices[0].MonikerString);
+            _videoSource.VideoResolution = SelectResolution(_videoSource);
+            _videoSource.NewFrame += VideoNewFrame;
+            _videoSource.Start();
+        }
+
+        /// <summary>
+        /// The Stop.
+        /// </summary>
+        public void Stop()
+        {
+            if (_videoSource is not { IsRunning: true }) return;
+
+            _videoSource.NewFrame -= VideoNewFrame;
+            _videoSource.SignalToStop();
+            _videoSource.WaitForStop();
+            _videoSource = null;
+        }
+
+        /// <summary>
+        /// The image convert callback.
+        /// </summary>
+        /// <returns>The <see cref="bool"/>.</returns>
+        private static bool ImageConvertCallback()
+        {
+            return false;
+        }
+
+        /// <summary>
+        /// The SelectResolution.
+        /// </summary>
+        /// <param name="device">The device<see cref="VideoCaptureDevice"/>.</param>
+        /// <returns>The <see cref="VideoCapabilities"/>.</returns>
+        private static VideoCapabilities SelectResolution(VideoCaptureDevice device)
+        {
+            foreach (var cap in device.VideoCapabilities)
+            {
+                if (cap.FrameSize.Height == 1080)
+                    return cap;
+                if (cap.FrameSize.Width == 1920)
+                    return cap;
+            }
+            return device.VideoCapabilities.Last();
+        }
+
+        /// <summary>
+        /// Get the cam list.
+        /// </summary>
+        /// <returns>The <see cref="FilterInfoCollection"/>.</returns>
+        private FilterInfoCollection GetCamList()
+        {
+            _videoDevices =  new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            return _videoDevices;
+        }
+
+        /// <summary>
+        /// Event handler if new frame is ready.
+        /// </summary>
+        /// <param name="sender">The sender<see cref="object"/>.</param>
+        /// <param name="eventArgs">The eventArgs<see cref="NewFrameEventArgs"/>.</param>
+        private void VideoNewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            this.CurrentImage = (Bitmap)eventArgs.Frame.GetThumbnailImage(_frameSize.Width, _frameSize.Height, ImageConvertCallback, IntPtr.Zero);
+        }
+    }
+}
