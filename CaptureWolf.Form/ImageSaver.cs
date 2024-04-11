7d77696f1e496790265c146e09fcb3664cebf853
Mark Heinis <mheinis@schubergphilis.com>
2024-04-11 11:13:09 +0200

    Add overlay to image

---
+++ CaptureWolf.Form/ImageSaver.cs
@@ -0,0 +1,57 @@
+﻿using System.ComponentModel;
+
+namespace CaptureWolf.UI;
+
+public class ImageSaver
+{
+    private readonly BackgroundWorker _worker = new BackgroundWorker();
+    private EventHandler<RunWorkerCompletedEventArgs> _whenCompleted;
+
+    public ImageSaver()
+    {
+        _worker.DoWork += Worker_DoWork;
+        _worker.RunWorkerCompleted += Worker_RunWorkerCompleted;
+    }
+
+    public void SaveImage(string fileName, Image image)
+    {
+        _worker.RunWorkerAsync(Tuple.Create(fileName, image));
+    }
+
+    private static void Worker_DoWork(object sender, DoWorkEventArgs e)
+    {
+        var args = (Tuple<string, Image>)e.Argument;
+        var fileName = args?.Item1;
+        var image = args?.Item2;
+
+        if(string.IsNullOrEmpty(fileName) || image == null)
+            return;
+
+        using var watermarkImage = Image.FromFile("icon.png");
+        var newHeight = image.Height / 3;
+        var newWidth = (int)(watermarkImage.Width * ((double)newHeight / watermarkImage.Height));
+        var resizedWatermarkImage = new Bitmap(watermarkImage, new Size(newWidth, newHeight));
+
+        using var imageWithWatermark = new Bitmap(image);
+
+        using var graphics = Graphics.FromImage(imageWithWatermark);
+        var watermarkPosition = new Point(imageWithWatermark.Width - resizedWatermarkImage.Width - 20, 20);
+        var pen = new Pen(Color.FromArgb(0, 0, 23), 175);
+        graphics.DrawRectangle(pen, 0, 0, imageWithWatermark.Width - 1, imageWithWatermark.Height - 1);
+        graphics.DrawImage(resizedWatermarkImage, watermarkPosition);
+
+        imageWithWatermark.Save(fileName);
+        e.Result = fileName;
+    }
+
+
+    private void Worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+    {
+        _whenCompleted?.Invoke(this, e);
+    }
+
+    public void SetOnCompletedEvent(EventHandler<RunWorkerCompletedEventArgs> args)
+    {
+        _whenCompleted = args;
+    }
+}
